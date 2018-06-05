81b8ea2c3348973a32316ada52d6cdb697351128
falcon-islas <38935950+falcon-islas@users.noreply.github.com>
2018-06-05 11:31:08 -0700

    lesson 6

---
+++ assignments/lesson-6/scripts/hamburger.js
@@ -0,0 +1,9 @@
+function toggleNavMenu() {
+    if (document.getElementById("menu").className == "") {
+        document.getElementById("menu").className = "show";
+    }
+    else
+    {
+        document.getElementById("menu").className = "";
+    } 
+}
